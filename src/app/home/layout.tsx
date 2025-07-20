4b18231b42a6fd23cba9a738c242cd1911f92341
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:27:10 +0700

    refactor: update color in navbar

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-[#F3F3E0] fixed w-full z-20 top-0 start-0 border--[#CBDCEB]">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a
               href="/"
@@ -31,7 +31,7 @@ export default function RootLayout({
                 className="h-8"
                 alt="Logo"
               />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#133E87]">
                 A-Ing!
               </span>
             </a>
@@ -39,7 +39,7 @@ export default function RootLayout({
               <a href="/main">
                 <button
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-[#133E87] hover:bg-[#608BC1] focus:ring-4 focus:outline-none focus:ring-[#CBDCEB] font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Get started
                 </button>
@@ -49,11 +49,11 @@ export default function RootLayout({
               className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
               id="navbar-sticky"
             >
-              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-[#CBDCEB] rounded-lg bg-[#CBDCEB] md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#F3F3E0]">
                 <li>
                   <a
                     href="/home"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                    className="block py-2 px-3 text-white bg-[#133E87] rounded-sm md:bg-transparent  md:p-0 md:text-[#133E87] md:hover:text-[#608BC1]"
                     aria-current="page"
                   >
                     Home
@@ -62,7 +62,7 @@ export default function RootLayout({
                 <li>
                   <a
                     href="/about"
-                    className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    className="block py-2 px-3 text-[#133E87] rounded-sm hover:bg-[#CBDCEB] md:hover:bg-transparent md:p-0 md:hover:text-[#608BC1]"
                   >
                     About
                   </a>
