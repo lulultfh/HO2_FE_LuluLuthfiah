03f59d27b44fdd6c686bc7b8026a5803d3e93a28
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:41:18 +0700

    feat: replace static text with the animated text

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -20,7 +20,7 @@ export default function Home(){
 
     return(
         <section className="p-8 text-center">
-            <h1 className="text-3xl font-bold mb-4">Welcome to A-Ing <span>Tic-Tac-Toe!</span></h1>
+            <h1 className="text-3xl font-bold mb-4">Welcome to A-Ing <span>{text}</span></h1>
         </section>
     )
 }
