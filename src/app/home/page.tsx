4a3f9db73033a01aac23a1b83250c3bceaab3f41
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:35:08 +0700

    chore: setup state for dynamic text

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,11 @@
 'use-client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function Home(){
+    const fullText = 'Tic-Tac-Toe!';
+    const [text, setText] = useState('');
+    
     return(
         <section className="p-8 text-center">
             <h1 className="text-3xl font-bold mb-4">Welcome to A-Ing <span>Tic-Tac-Toe!</span></h1>
