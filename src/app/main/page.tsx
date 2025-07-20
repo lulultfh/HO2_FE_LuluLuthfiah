de47fabf9611ae112f39a60baddaf873db3b4785
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:56:50 +0700

    feat: define winning condition for the game

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -6,4 +6,15 @@ export default function TicTacToe() {
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
   const [gameState, setGameState] = useState<string[]>(Array(9).fill(""));
   const [statusMessage, setStatusMessage] = useState<string>("It's X's turn!");
+
+  const winningConditions: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 }
