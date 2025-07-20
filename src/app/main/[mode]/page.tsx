9d8ea426f47ef884646dcf84c90287394026574a
lululthfh <punyalulu05@gmail.com>
2025-07-20 20:34:31 +0700

    feat: import param to access dynamic param route

--- src/app/main/[mode]/page.tsx
+++ src/app/main/[mode]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import { useParams } from 'next/navigation';
 
 export default function TicTacToe() {
   const [gameActive, setGameActive] = useState(true);
