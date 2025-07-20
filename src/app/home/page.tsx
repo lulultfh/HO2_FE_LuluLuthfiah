c092e10534252c982d108bac81139de940e3a72e
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:39:27 +0700

    feat: implement typing animation logic with useEffect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,19 @@ import { useEffect, useState } from "react";
 export default function Home(){
     const fullText = 'Tic-Tac-Toe!';
     const [text, setText] = useState('');
-    
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const interval = setInterval(()=> {
+            setText(fullText.slice(0, currentIndex + 1));
+            currentIndex++;
+            if(currentIndex === fullText.length){
+                clearInterval(interval);
+            }
+        }, 100);
+        return () => clearInterval(interval);
+    }, []);
+
     return(
         <section className="p-8 text-center">
             <h1 className="text-3xl font-bold mb-4">Welcome to A-Ing <span>Tic-Tac-Toe!</span></h1>
