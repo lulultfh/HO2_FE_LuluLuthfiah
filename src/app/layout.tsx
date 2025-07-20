d716833c36e38f27a1a1a00772e19d54eb18f547
lululthfh <punyalulu05@gmail.com>
2025-07-20 20:06:17 +0700

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,60 @@ export default function RootLayout({
             </div>
           </div>
         </nav>
-        <div className="flex items-center justify-center min-h-screen">{children}</div>
+        <div className="flex items-center justify-center min-h-screen">
+          {children}
+        </div>
+
+        <footer className="bg-[#F3F3E0] dark:bg-gray-900">
+          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="md:flex md:justify-between">
+              <div className="mb-6 md:mb-0">
+                <a href="/" className="flex items-center">
+                  <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                    A-Ing
+                  </span>
+                </a>
+              </div>
+              <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                <div>
+                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                    Resources
+                  </h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="mb-4">
+                      <a
+                        href="https://flowbite.com/"
+                        className="hover:underline"
+                      >
+                        Flowbite
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://tailwindcss.com/"
+                        className="hover:underline"
+                      >
+                        Tailwind CSS
+                      </a>
+                    </li>
+                  </ul>
+                </div>                
+              </div>
+            </div>
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © {new Date().getFullYear()}{" "}
+                <a href="/" className="hover:underline">
+                  A-Ing™
+                </a>
+                . All Rights Reserved.
+              </span>
+              <div className="flex mt-4 sm:justify-center sm:mt-0 space-x-5">
+              </div>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
