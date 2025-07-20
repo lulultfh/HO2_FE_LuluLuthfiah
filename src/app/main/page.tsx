9ee9fc2c055beb4c363b4aa5c3b4410aa93be555
lululthfh <punyalulu05@gmail.com>
2025-07-20 18:55:58 +0700

    feat: initialize game state and logic variables

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,6 +1,9 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
-export default function TicTacToe(){
-    
+export default function TicTacToe() {
+  const [gameActive, setGameActive] = useState(true);
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
+  const [gameState, setGameState] = useState<string[]>(Array(9).fill(""));
+  const [statusMessage, setStatusMessage] = useState<string>("It's X's turn!");
 }
