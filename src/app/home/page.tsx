60b828c5884efee722f346802520a11c61f2d155
lululthfh <punyalulu05@gmail.com>
2025-07-20 15:57:42 +0700

    feat: add home page component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,1 +1,9 @@
 import "./globals.css"
+
+export default function Home(){
+    return(
+        <section className="p-8 text-center">
+            <h1 className="text-3xl font-bold mb-4">Welcome to A-Ing <span>Tic-Tac-Toe!</span></h1>
+        </section>
+    )
+}
